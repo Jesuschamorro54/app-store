4714e5c0d1586ae30282315b68720db85d7ec6f3
Jesus chamorro <jesusfeli54@gmail.com>
2023-07-15 12:37:30 -0500

    firts commit

---
+++ controllers.py
@@ -0,0 +1,18 @@
+import json
+import functools
+from flask import ( render_template, redirect, blueprints, session, request, url_for,)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from database.database import *
+
+main = blueprints.Blueprint('main', __name__)
+
+@main.context_processor
+def general_variables():
+    return { 'appName': "Tiendita" }
+
+
+@main.route('/', methods=['GET'])
+def index():
+    return render_template('/index.html')
+
