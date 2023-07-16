4a1c223bd5379fc474971876998644dfdfcbe29a
Jesus chamorro <jesusfeli54@gmail.com>
2023-07-15 22:23:47 -0500

    nav-bar terminada

--- routes.py
+++ routes.py
@@ -6,7 +6,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from database.database import *
 
 
-
 main = blueprints.Blueprint('main', __name__)
 
 @main.context_processor
@@ -14,8 +13,15 @@ def general_variables():
     return { 'appName': "Tiendita" }
 
 
+# ------------------------ Template Rendering ------------------------
 @main.route('/', methods=['GET'])
 def index():
+    return redirect(url_for('main.home'))
+
+@main.route('/home', methods=['GET'])
+def home():
     return render_template('/index.html')
+
+
 
 
