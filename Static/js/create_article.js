61740d8dee116bd58393e1c94c248def99e8093b
Alfredo112503 <alfredcomas8@gmail.com>
2023-08-15 17:17:21 -0500

    distributor problem fixed

--- Static/js/create_article.js
+++ Static/js/create_article.js
@@ -1,7 +1,7 @@
 "use strict";
 
-$(document).ready(function() {
-    $("#submit-button").click(function() {
+$(document).ready(function () {
+    $("#submit-button").click(function () {
         // Obtener los datos del formulario
         var formData = $("#create-article-form").serialize();
         const data = decodeBinaryData(formData);
@@ -16,13 +16,13 @@ function decodeBinaryData(formData) {
     let data = {}
 
     const isNumberField = (field) => {
-        const fields =  ['sale', 'price']
+        const fields = ['sale', 'price']
         return Boolean(fields.includes(field))
     }
 
     decodeURIComponent(formData).split("&").forEach((item) => {
-        const [field, value ] = item.split("=")
-        if (value) data[field] = isNumberField(field) ? parseFloat(value) : value
+        const [field, value] = item.split("=")
+        if (value && field != "provider") data[field] = isNumberField(field) ? parseFloat(value) : value
     })
 
     return data
